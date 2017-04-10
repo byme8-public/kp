3c94167c5af4ea48340d4ceb9bc4818fa01a22d6
Stanislav Silin <byme.work@gmail.com>
2017-04-10 13:21:45 +0300

    Update window style

--- kp/MainWindow.xaml.cs
+++ kp/MainWindow.xaml.cs
@@ -11,7 +11,7 @@ namespace kp
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow
 	{
 		public MainWindow()
 		{
